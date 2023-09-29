c2a04148ae0b0992758c0a0fb5b0f8b4e98b6a2e
hongjiaherng <jiaherng2001@gmail.com>
2023-09-29 22:39:18 +0800

    Reduce train_batch_size to avoid CUDA out of memory

--- segmentation/src/train.py
+++ segmentation/src/train.py
@@ -139,7 +139,7 @@ if __name__ == "__main__":
     # Training configs
     train_configs = {
         "max_epochs": 1000,
-        "train_batch_size": 8,
+        "train_batch_size": 6,
         "test_batch_size": 1,
         "learning_rate": 1e-3,
         "weight_decay": 1e-5,
